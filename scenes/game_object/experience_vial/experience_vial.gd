4be58706bf84bbc05f268757467c467ce7b8d6d8
bartoszpasiecznik <40137554+bartoszpasiecznik@users.noreply.github.com>
2024-02-02 23:54:31 +0100

    Added SFX

--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -40,4 +40,5 @@ func on_area_entered(other_area: Area2D):
 	tween.tween_property(sprite, "scale", Vector2.ZERO, .1).set_delay(.4)
 	tween.chain()
 	tween.tween_callback(collect)
+	$RandomStreamPlayer2DComponent.play_random()
 	
